02b9e71f4c13c613a5cb66be752747990d0873d8
netw3rker <moodle_porter@yarooze.com>
2014-04-04 12:36:42 +0800

    Establishing a 2.5.x compatibility branch. The version information should not be updated in this branch

--- version.php
+++ version.php
@@ -3,5 +3,5 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version = 2013110500; // The current plugin version (Date: YYYYMMDDXX)
-$plugin->requires = 2013110500; // Requires this Moodle version
+$plugin->requires = 2011110500; // Requires this Moodle version
 $plugin->component = 'auth_drupalservices'; // Full name of the plugin (used for diagnostics)
